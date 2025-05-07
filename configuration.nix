cf22d2706eb366749f3996aaee4d7118e1dc984d
Evandro Junior <evandrojr@gmail.com>
2025-05-06 21:50:36 -0300

    fix: correct display manager autoLogin user configuration

--- configuration.nix
+++ configuration.nix
@@ -100,7 +100,7 @@
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "dg";
+  services.displayManager.autoLogin.user = "dg";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
