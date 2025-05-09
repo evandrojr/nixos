096f426f8843c688016888c0a9785f5fe85f3f6f
Evandro Junior <evandrojr@gmail.com>
2025-05-09 09:16:02 -0300

    separando arquivos p2

--- hosts-mara/settings.nix
+++ hosts-mara/settings.nix
@@ -1,41 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ../common.nix
-      ./hardware-configuration.nix
-    ];
 
-  # Host-specific configurations can be added here.
-  networking.hostName = "mara";
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Bahia";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "pt_BR.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
-
-  # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = false;
   services.displayManager.defaultSession = "xfce";
@@ -52,99 +21,5 @@
     variant = "";
   };
 
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.dg = {
-    shell = pkgs.zsh;
-    isNormalUser = true;
-    description = "Evandro";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
-  };
-
-  security.sudo = {
-    enable = true;
-    wheelNeedsPassword = false;
-  };
-
-  programs.nix-ld.enable = true;
-
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "dg";
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    go
-    vscode
-    google-chrome
-    docker
-    zed
-    qbittorrent
-    vlc
-    fzf
-    libreoffice
-    webtorrent_desktop
-    gnome-tweaks
-    python3
-    temurin-jre-bin-24
-    direnv
-    vscode-extensions.mkhl.direnv
-    psmisc
-    apacheHttpd
-    tig
-    rustdesk
-    xreader
-  ];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableBashCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    ohMyZsh = {
-      enable = true;
-      theme = "agnoster";
-    };
-  };
-
-  virtualisation.docker.enable = true;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  system.stateVersion = "24.11";
+ 
 }
