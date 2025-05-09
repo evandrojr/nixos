270cfddf6e902b018358c8ffeca3faf0ac12a314
Evandro Junior <evandrojr@gmail.com>
2025-05-09 09:22:02 -0300

    separando arquivos p3

--- configuration.nix
+++ configuration.nix
@@ -53,12 +53,6 @@ in {
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "br";
--- hosts-amor-gera-amor/settings.nix
+++ hosts-amor-gera-amor/settings.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  # Enable the GNOME Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
 
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
 
 }
