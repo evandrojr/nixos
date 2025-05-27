c83138b1b10fcda371e465bfac235047fa41d8ca
Evandro Junior <evandrojr@gmail.com>
2025-05-27 07:25:22 -0300

    limpeza

--- configuration.nix
+++ configuration.nix
@@ -143,16 +143,6 @@ in {
     tig
     xreader
     nmap
-    hyprland
-    # hyprland-garuda
-    hyprpaper
-    hyprpicker
-    # hyprland-scripts
-    # hyprland-scripts-hyprpicker
-    # hyprland-scripts-hyprpaper
-    ventoy-full-gtk
-    pxz
-    sabnzbd
     nemo
   ] ++(config.myExtraPackages or []);
 
@@ -181,25 +171,6 @@ in {
       RemainAfterExit = true;
     };
   };
-
-  # Script para configurar atalhos com tecla Windows
-  environment.etc."profile.d/gnome-shortcuts.sh".text = ''
-    if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
-      gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/']"
-      
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>t'
-      
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'File Manager'
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'nemo'
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>e'
-      
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'VS Code'
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ command 'code'
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ binding '<Super>x'
-    fi
-  '';
 
 
   # This value determines the NixOS release from which the default
